8aa59d3499805946c1be8042bd2e020aa4e08c89
mwangemi <mwangemik@gmail.com>
2021-06-22 07:40:06 +0300

    add sidebar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -16,10 +16,12 @@ const Topbar = () => {
                </div>
                <div className="topbarIconContainer">
                   <Language />
+                  <span className="topIconBadge">2</span>
                </div>
                <div className="topbarIconContainer">
                   <Settings />
                </div>
+               <img src="https://media.istockphoto.com/photos/lion-in-high-grass-picture-id494856046?k=6&m=494856046&s=612x612&w=0&h=Eb3uv75Vld5c_n22v8i5XktUPl60czynY4EVx7XMuV4=" alt="" className="topAvatar" />
             </div>
          </div>
       </div>
