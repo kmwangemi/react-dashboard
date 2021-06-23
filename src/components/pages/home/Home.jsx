b0c9c897efe9bbb54b160de15f77ac894400b5f9
mwangemi <mwangemik@gmail.com>
2021-06-23 20:13:51 +0300

    add chart and cards to home page

---
+++ src/components/pages/home/Home.jsx
@@ -0,0 +1,20 @@
+import Chart from '../../../chart/Chart'
+import FeaturedInfo from '../../featuredInfo/FeaturedInfo'
+import './home.css'
+import { userData } from '../../../dummyData';
+
+const Home = () => {
+   return (
+      <div className="home">
+         <FeaturedInfo/>
+         <Chart 
+            data={userData}
+            title="User Analytics"
+            grid
+            dataKey="Active User"
+         />
+      </div>
+   )
+}
+
+export default Home
